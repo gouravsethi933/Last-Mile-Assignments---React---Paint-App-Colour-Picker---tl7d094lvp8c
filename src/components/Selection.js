df679b0d48b176e60c47587c2855548482d596ea
Gourav Sethi <61932647+gouravsethi933@users.noreply.github.com>
2022-03-07 13:18:21 +0530

    lastmile

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 export default function Selection({ applyColor }) {
   let [color, setColor] = useState({ background: "" });
+
   return (
     <>
       <div
